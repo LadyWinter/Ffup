23cf2ffdf0593e59dd5ce6da04c4880955a5f1ec
LadyWinter <33619469+LadyWinter@users.noreply.github.com>
2017-11-13 23:03:06 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.Mzc4Njg4MTg5MTM4NzMxMDA4.DOr9Mw.2nNF_0X4kdf6foHLiBKs44xRpxg);
